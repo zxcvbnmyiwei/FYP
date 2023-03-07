07d11a094d36bba370b902407f0d02d5daba88a2
zxcvbnmyiwei <cyiwei1998@gmail.com>
2023-03-07 21:16:09 +0800

    changed settings.py

--- backend/main/main/settings.py
+++ backend/main/main/settings.py
@@ -35,10 +35,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000","https://stupendous-gnome-fe5fe3.netlify.app/",
 ]
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-# ]
-
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_COOKIE_SECURE = False
